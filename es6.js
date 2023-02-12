92def5b1f14796c8b79bf3bef537d9214d20c8a6
emwhylaj <olalekanolawale1307@gmail.com>
2023-02-12 10:58:08 +0100

    Rest Operator

--- es6.js
+++ es6.js
@@ -17,5 +17,22 @@ const greetings = (name, age) =>{
     return `Welcome ${name} ${age}`;
 }
 
-console.log(greetings('Muyiwa', 35));
+//console.log(greetings('Muyiwa', 35));
 
+//Rest and Spread Operator
+
+//const mSum = (num1, num2) => console.log(num1 + num2);
+
+const mSum = (num1, num2, ... args) => {
+    console.log(num1);
+    console.log(num2);
+    console.log(args);
+    let sum = num1 + num2;
+    for(i = 0;i < args.length; i++){
+        sum += args[i];
+    }
+
+    console.log(sum)
+}
+
+mSum(1,2,3,4,5,6,7);
